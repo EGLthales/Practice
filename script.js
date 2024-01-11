bfff99cfb94d41b6a0c11b78ddcf4cb4ed72467e
Charles AV <charlesav22@gmail.com>
2024-01-11 14:22:56 -0300

    update avatars

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function toggleMode(){
     /*TROCA DE IMAGEM */
     if (html.classList.contains('light')){
         img.setAttribute('src','./assets/avatar-light.png')
-        img.setAttribute('alt','fundo amarelo')
+        img.setAttribute('alt','fundo azul')
     }else{
         img.setAttribute('src','./assets/avatar.png')
-        img.setAttribute('alt','fundo azul')
+        img.setAttribute('alt','fundo roxo')
     }
     
 }
